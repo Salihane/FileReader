095acc005f0a19d0797726b24c9f400fb7f79d4b
salim.benhadou <Salim.BenHadou@tobania.be>
2020-03-23 21:43:46 +0100

    Version3: - Creating encrypted text file reader. - Unit testing the encrypted text file reader.

---
+++ FileReader/FileReader.Lib.Tests/EncryptedFileReaderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileReader.Lib.Encryption;
+using FileReader.Lib.Helpers;
+using FileReader.Lib.Readers;
+using Xunit;
+
+namespace FileReader.Lib.Tests
+{
+    public class EncryptedFileReaderTests
+    {
+		[Fact]
+		public async Task Read_EncryptedTextFile_ShouldReturnDecryptedFileContent()
+		{
+			// Arrange
+			var dir = TestHelper.GetFilesDirectory();
+			var path = Path.Combine(dir, "encryptedtextfile.txt");
+			var textFileReader = new TextFileReader(path, new FileValidator());
+			var sut = new EncryptedFileReader(textFileReader, new ReverseEncryption());
+			const string expectedFileContent = "Hello there!\r\nThis is an encrypted text file.\r\n\r\n\r\n";
+
+			// Act
+			var fileContent = await sut.ReadAsync();
+
+			// Assert
+			Assert.Equal(expectedFileContent, fileContent);
+		}
+	}
+}
---
+++ FileReader/FileReader.Lib/Encryption/ReverseEncryption.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileReader.Lib.Encryption
+{
+	public class ReverseEncryption : EncryptionStrategy
+	{
+		private const string WordsSeparator = " ";
+
+		public override string Decrypt(string content)
+		{
+			if (string.IsNullOrEmpty(content))
+			{
+				return content;
+			}
+
+			var lines = content.Split(Environment.NewLine);
+			var reversedContent = ReverseLines(lines);
+			return reversedContent;
+		}
+
+		private static string ReverseWord(string word)
+		{
+			if (string.IsNullOrEmpty(word) || word.Length == 1)
+			{
+				return word;
+			}
+
+			var chars = word.ToCharArray();
+			Array.Reverse(chars);
+			return new string(chars);
+		}
+
+		private static string ReverseLine(string line)
+		{
+			if (string.IsNullOrEmpty(line) || line.Length == 1)
+			{
+				return line;
+			}
+
+			var words = line.Split(WordsSeparator);
+			var builder = new StringBuilder();
+			foreach (var word in words)
+			{
+				builder.Append($"{ReverseWord(word)}{WordsSeparator}");
+			}
+
+			return builder.ToString().TrimEnd();
+		}
+
+		private static string ReverseLines(IReadOnlyList<string> lines)
+		{
+			var builder = new StringBuilder();
+			for (var i = 0; i < lines.Count; i++)
+			{
+				if (i != 0)
+				{
+					builder.Append(Environment.NewLine);
+				}
+
+				builder.Append(ReverseLine(lines[i]));
+			}
+
+			return builder.ToString();
+		}
+	}
+}
