bbf99fc5080bf3b8995c6ecc8bdf0043c67af864
salim.benhadou <Salim.BenHadou@tobania.be>
2020-03-24 11:49:02 +0100

    Version 4: - Creating a secured file reader based on user roles. - Unit testing the secured reader for xml files.

---
+++ FileReader/FileReader.Lib/Authorization/ReadAuthorization.cs
@@ -0,0 +1,27 @@
+﻿using FileReader.Lib.Enums;
+using System.IO;
+
+namespace FileReader.Lib.Authorization
+{
+	public class ReadAuthorization : RoleBasedAuthorization
+	{
+		public ReadAuthorization(UserRole userRole)
+		{
+			UserRole = userRole;
+		}
+
+		// This is a dummy implementation to demonstrate an authorization process
+		public override bool IsAuthorized(string filePath)
+		{
+			var fileName = Path.GetFileName(filePath);
+
+			return UserRole switch
+			{
+				UserRole.Admin => true,
+				UserRole.Consultant => (fileName.Length > 10),
+				UserRole.Accountant => (fileName.Length < 15),
+				_ => false
+			};
+		}
+	}
+}
