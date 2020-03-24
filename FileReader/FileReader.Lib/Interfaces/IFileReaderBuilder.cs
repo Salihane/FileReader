1db6f02c0229443805b3b615beb271554ad71f0f
salim.benhadou <Salim.BenHadou@tobania.be>
2020-03-24 14:44:09 +0100

    Bonus: - Implementing a file reader builder - Unit testing the file reader builder

---
+++ FileReader/FileReader.Lib/Interfaces/IFileReaderBuilder.cs
@@ -0,0 +1,14 @@
+﻿using FileReader.Lib.Authorization;
+using FileReader.Lib.Encryption;
+using FileReader.Lib.Enums;
+
+namespace FileReader.Lib.Interfaces
+{
+	public interface IFileReaderBuilder
+	{
+		IFileReaderBuilder Init(FileType fileType);
+		IFileReaderBuilder UseEncryption(EncryptionStrategy encryptionStrategy);
+		IFileReaderBuilder UseAuthorization(RoleBasedAuthorization authorization);
+		Readers.FileReader Build();
+	}
+}
---
+++ FileReader/FileReader.Lib/Readers/FileReaderBuilder.cs
@@ -0,0 +1,71 @@
+﻿using FileReader.Lib.Authorization;
+using FileReader.Lib.Encryption;
+using FileReader.Lib.Enums;
+using FileReader.Lib.Helpers;
+using FileReader.Lib.Interfaces;
+
+namespace FileReader.Lib.Readers
+{
+	public class FileReaderBuilder : IFileReaderBuilder
+	{
+		private readonly IFileValidator _fileValidator;
+		private readonly string _filePath;
+		private FileReader _fileReader;
+
+		public FileReaderBuilder(string filePath,
+								 IFileValidator fileValidator)
+		{
+			_filePath = filePath;
+			_fileValidator = fileValidator;
+		}
+
+		public FileReaderBuilder(string filePath)
+			: this(filePath, new FileValidator())
+		{
+		}
+
+		public IFileReaderBuilder Init(FileType fileType)
+		{
+			InitFileReader(fileType);
+			return this;
+		}
+
+		public IFileReaderBuilder UseEncryption(EncryptionStrategy encryptionStrategy)
+		{
+			if (_fileReader == null)
+			{
+				InitFileReader(FileType.Text);
+			}
+
+			_fileReader = new EncryptedFileReader(_fileReader, encryptionStrategy);
+			return this;
+		}
+
+		public IFileReaderBuilder UseAuthorization(RoleBasedAuthorization authorization)
+		{
+			if (_fileReader == null)
+			{
+				InitFileReader(FileType.Text);
+			}
+
+			_fileReader = new SecuredFileReader(_fileReader, authorization);
+			return this;
+		}
+
+		public FileReader Build()
+		{
+			return _fileReader;
+		}
+
+		private void InitFileReader(FileType fileType)
+		{
+			_fileReader = fileType switch
+			{
+				FileType.Text => new TextFileReader(_filePath, _fileValidator),
+				FileType.Xml => new XmlFileReader(_filePath, _fileValidator),
+				FileType.Json => new JsonFileReader(_filePath, _fileValidator),
+				_ => _fileReader
+			};
+		}
+	}
+}
