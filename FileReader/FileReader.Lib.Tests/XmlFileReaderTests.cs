6ef7efed1fd2162a9a402fdbc35d98ce479861cf
salim.benhadou <Salim.BenHadou@tobania.be>
2020-03-24 17:04:24 +0100

    Bonsus: fixing unit tests.

--- FileReader/FileReader.Lib.Tests/XmlFileReaderTests.cs
+++ FileReader/FileReader.Lib.Tests/XmlFileReaderTests.cs
@@ -42,7 +42,7 @@ namespace FileReader.Lib.Tests
 			var path = Path.Combine(dir, "regularxmlfile.xml");
 			var sut = new XmlFileReader(path, new FileValidator());
 			const string expectedFileContent =
-				"<?xml version=\"1.0\" encoding=\"utf-8\"?><customers><customer><name>Customer 1</name><Address><Street>Hello street</Street><HouseNr>87</HouseNr><Box>4b</Box><ZipCode>1000</ZipCode><City>Brussels</City></Address></customer><customer><name>Customer 2</name><Address><Street>Morning street</Street><HouseNr>5</HouseNr><Box>10</Box><ZipCode>9900</ZipCode><City>Gent</City></Address></customer></customers>";
+				"<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<customers>\r\n\t<customer>\r\n\t\t<name>Customer 1</name>\r\n\t\t<Address>\r\n\t\t\t<Street>Hello street</Street>\r\n\t\t\t<HouseNr>87</HouseNr>\r\n\t\t\t<Box>4b</Box>\r\n\t\t\t<ZipCode>1000</ZipCode>\r\n\t\t\t<City>Brussels</City>\r\n\t\t</Address>\r\n\t</customer>\r\n\t<customer>\r\n\t\t<name>Customer 2</name>\r\n\t\t<Address>\r\n\t\t\t<Street>Morning street</Street>\r\n\t\t\t<HouseNr>5</HouseNr>\r\n\t\t\t<Box>10</Box>\r\n\t\t\t<ZipCode>9900</ZipCode>\r\n\t\t\t<City>Gent</City>\r\n\t\t</Address>\r\n\t</customer>\r\n</customers>";
 
 			// Act
 			var fileContent = await sut.ReadAsync();
